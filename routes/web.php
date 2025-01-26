2401bb40114f9a34e4040dcb8f2d22ab82177046
Mervin <jmeva.quimio12345@gmail.com>
2025-01-26 18:46:45 +0800

    Added templated html (1/26)

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForumController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ForumController::class, 'forum'])->name('forum');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
